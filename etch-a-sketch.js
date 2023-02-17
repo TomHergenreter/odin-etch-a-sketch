c60591377dc68bee91cfb05e161b5fe414005cdc
Tom Hergenreter <4270511+TomHergenreter@users.noreply.github.com>
2023-02-17 15:32:15 -0700

    Add random color function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,9 +1,8 @@
-const paintColor = '#000000'
 const colorRandom = document.querySelector('#colorBlackButton');
 const colorBlack = document.querySelector('#colorRandomButton')
 colorBlack.addEventListener('click', setPaintColor);
 colorRandom.addEventListener('click', setPaintColor);
-
+let paintColor = '#000000'
 
 function getGridSize() {
     const gridSize = prompt('Enter grid size');
@@ -20,7 +19,7 @@ function createGrid(gridSize) {
     `;
     for (let i = 0; i < (gridSize * gridSize); i++){
         const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-test');
+        gridItem.classList.add('grid-item');
         gridItem.addEventListener('click', paintGridItem);
         gridItem.addEventListener('mousemove', paintGridItem);
         gridContainer.append(gridItem);
@@ -28,16 +27,27 @@ function createGrid(gridSize) {
 }
 
 function setPaintColor(event) {
-    const color = event.target.getAttribute('data-color')
+    const color = event.target.getAttribute('data-color');
+    switch (color) {
+        case 'black' : paintColor = '#000000';
+        break;
+        case 'random' : paintColor = `#${getRandomColor()}`;
+        break;
+    }
     console.log(color);
 }
 
 function paintGridItem (event){
     if (event.type === 'click'){
-        event.target.style.backgroundColor = '#000000';
+        event.target.style.backgroundColor = paintColor;
     } else if(event.type === 'mousemove' && event.buttons === 1){
-        event.target.style.backgroundColor = '#000000';
+        event.target.style.backgroundColor = paintColor;
     } 
+}
+
+function getRandomColor(){
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    return randomColor;
 }
 
 function initialize() {
